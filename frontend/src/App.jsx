5798d375dfe130d128c082de0cc73242871ef6ab
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-12-27 10:48:29 +0100

    Create main page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,19 @@ function App() {
 
   return (
     <div className="App">
-      <h1>App</h1>
+      <header>
+        <h1 className="logo">Ctrl DB</h1>
+
+        <nav></nav>
+      </header>
+      
+      <main>
+        <h2>App</h2>
+
+        <footer>
+          &copy; 2021 Sebastian Mineur
+        </footer>
+      </main>
     </div>
   )
 }
