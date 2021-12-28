fcacf4bdd4c9950d4589f613cd91facf56d1b863
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-12-28 11:55:01 +0100

    Make Navbar dark

--- frontend/src/pages/partials/Navbar.jsx
+++ frontend/src/pages/partials/Navbar.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <header className="bg-white shadow-lg p-1">
-            <div className="container-lg flex align-center justify-between">
-                <h1 className="font-lg m-0">Ctrl DB</h1>
+  return (
+    <header className="Navbar py-2 px-1">
+      <div className="flex align-center justify-between">
+        <h1 className="font-lg m-0">Ctrl DB</h1>
 
-                <nav className="flex gap-1">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="about">About</NavLink>
-                </nav>
-            </div>
-        </header>
-    );
+        <nav className="flex gap-1">
+          <NavLink to="/" className="col-white">
+            Home
+          </NavLink>
+          <NavLink to="device" className="col-white">
+            Device
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
