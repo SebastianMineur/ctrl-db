82ea1d8b379c7d75a11ca063cb03aea4a7cf30f6
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-12-29 12:05:11 +0100

    Update navbar/navmenu based on user state

--- frontend/src/pages/partials/Navbar.jsx
+++ frontend/src/pages/partials/Navbar.jsx
@@ -4,18 +4,24 @@ import Icon from "../../components/Icon";
 import { menu, menuOpen } from "../../assets/icons";
 import "./Navbar.css";
 import Navmenu from "./Navmenu";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState();
+  const { currentUser } = useAuthContext();
 
   return (
     <header className="Navbar py-2 px-1">
       <div className="flex align-center justify-between">
         <h1 className="font-lg m-0">Ctrl DB</h1>
 
-        <Button className="col-white" onClick={() => setShowMenu(!showMenu)}>
-          <Icon icon={showMenu ? menuOpen : menu} size="2rem" />
-        </Button>
+        <div className="flex align-center gap-1">
+          {currentUser && <span>{currentUser.username}</span>}
+
+          <Button className="col-white" onClick={() => setShowMenu(!showMenu)}>
+            <Icon icon={showMenu ? menuOpen : menu} size="2rem" />
+          </Button>
+        </div>
       </div>
 
       {showMenu && <Navmenu />}
--- frontend/src/pages/partials/Navmenu.jsx
+++ frontend/src/pages/partials/Navmenu.jsx
@@ -1,25 +1,38 @@
 import { NavLink } from "react-router-dom";
+import Button from "../../components/Button";
 import Icon from "../../components/Icon";
-import { home } from "../../assets/icons";
+import { home, login, plus } from "../../assets/icons";
 import "./Navmenu.css";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 const Navmenu = () => {
+  const { currentUser, logout } = useAuthContext();
+
   return (
     <nav className="Navmenu flex column gap-1 bg-white p-1 rounded shadow-lg">
-      <NavLink to="/" className="Navlink">
+      <NavLink to="/">
         Home
-        <Icon icon={home} />
+        <Icon icon={home} size="1.25em" />
       </NavLink>
 
-      <NavLink to="device" className="Navlink">
-        Device
-        <Icon icon={home} />
+      <NavLink to="device">
+        Create device
+        <Icon icon={plus} size="1.25em" />
       </NavLink>
 
-      <NavLink to="login" className="Navlink">
-        Log in
-        <Icon icon={home} />
-      </NavLink>
+      <hr />
+
+      {currentUser ? (
+        <Button onClick={logout}>
+          Log out
+          <Icon icon={login} size="1.25em" />
+        </Button>
+      ) : (
+        <NavLink to="login">
+          Log in
+          <Icon icon={login} size="1.25em" />
+        </NavLink>
+      )}
     </nav>
   );
 };
