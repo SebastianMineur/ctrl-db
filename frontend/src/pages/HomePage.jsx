33790a98140522cd67da441989069f398eed9c41
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-12-29 15:34:23 +0100

    Update HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import { search } from "../services/strapi";
 import "./HomePage.css";
 
@@ -24,36 +25,70 @@ const HomePage = () => {
   };
 
   return (
-    <div className="HomePage container-lg">
-      <h2 className="font-xl mt-1 mb-0">Find device</h2>
+    <div className="HomePage">
+      <form onSubmit={handleSubmit} className="container-md flex column gap-1">
+        <h2 className="font-xl mt-1 mb-0">Search devices</h2>
 
-      <p className="mt-0">Search by model, manufacturer or device type.</p>
+        <input
+          ref={searchRef}
+          className="bg-white"
+          placeholder="Model, manufacturer or type..."
+        />
 
-      <form onSubmit={handleSubmit} className="flex column gap-05">
-        <input ref={searchRef} className="bg-white" />
+        <div className="flex justify-center align-center gap-1">
+          <label>Filters:</label>
+
+          <select className="bg-white">
+            <option value="" disabled>
+              Type
+            </option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+            <option>Option 4</option>
+          </select>
+
+          <select className="bg-white">
+            <option value="" disabled>
+              Manufacturer
+            </option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+            <option>Option 4</option>
+          </select>
+        </div>
+
+        <hr className="my-1" />
       </form>
 
-      {searchString && (
-        <h3 className="font-sm m-0 px-1 py-05 mt-1 text-center">
-          {results?.length
-            ? `${results.length} result(s) for '${searchRef.current.value}'`
-            : `No results for '${searchRef.current.value}'`}
-        </h3>
-      )}
+      <div className="container-lg">
+        {searchString && (
+          <h3 className="font-sm m-0 mb-05 text-center">
+            {results?.length
+              ? `${results.length} result(s) for '${searchRef.current.value}'`
+              : `No results for '${searchRef.current.value}'`}
+          </h3>
+        )}
 
-      {results?.length > 0 && (
-        <>
-          <div className="ResultsList bg-white rounded b-1">
-            {results.map((device) => (
-              <div key={device.id} className="SearchResult p-1">
-                <p className="m-0 font-bold">{device.manufacturer.name}</p>
-                <p className="m-0">{device.model}</p>
-                <p className="m-0 font-xs font-bold">{device.type}</p>
-              </div>
-            ))}
-          </div>
-        </>
-      )}
+        {results?.length > 0 && (
+          <>
+            <div className="ResultsList bg-white rounded b-1">
+              {results.map((device) => (
+                <div key={device.id} className="SearchResult p-1">
+                  <div className="flex justify-between">
+                    <Link to={`/device/${device.id}`} className="m-0 font-bold">
+                      {device.model}
+                    </Link>
+                    <p className="m-0">{device.type}</p>
+                  </div>
+                  <p className="m-0">{device.manufacturer.name}</p>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
