0825f2c79af371804df527589fe5ff2128676b6f
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-02-08 14:52:11 +0100

    Create Modal component

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,22 @@
+import { useRef } from "react";
+import "./Modal.css";
+
+const Modal = ({ children, className, onClick, ...props }) => {
+  const ref = useRef();
+
+  return (
+    <div
+      ref={ref}
+      className={`Modal ${className}`}
+      onClick={(e) => {
+        if (e.target !== ref.current) return;
+        if (typeof onClick === "function") onClick(e);
+      }}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Modal;
