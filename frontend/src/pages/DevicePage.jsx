6ce1cb6eb9ff31bc28e3c9122a58b20e39426294
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-02-08 22:15:16 +0100

    Fetch and display real device data on device page

--- frontend/src/pages/DevicePage.jsx
+++ frontend/src/pages/DevicePage.jsx
@@ -1,7 +1,24 @@
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { GET_DEVICE_FROM_ID } from "../queries/devices";
+
 const DevicePage = () => {
+  const { id } = useParams();
+  const deviceQuery = useQuery(GET_DEVICE_FROM_ID, { variables: { id } });
+
+  if (!deviceQuery.data) return null;
+
+  const brand =
+    deviceQuery.data.device.data.attributes.brand.data.attributes.name;
+  const model = deviceQuery.data.device.data.attributes.model;
+
   return (
     <div className="container-lg">
-      <h2 className="font-xl mt-1 mb-0">Device</h2>
+      <h2 className="font-xl mt-1 mb-0">
+        <span className="block font-md">{brand}</span>
+        {model}
+      </h2>
+
       <p className="mt-0">
         A device can be any piece of hardware capable of receiving control
         commands.
