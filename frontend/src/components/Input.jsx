8be40121dd25d33372f9ef8ae58ec55b0f977406
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-02-08 14:51:57 +0100

    Create Input component

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,15 @@
+import "./Input.css";
+
+const Input = ({ className, error, ...props }) => {
+  return (
+    <>
+      <input
+        className={`Input ${error ? "error" : ""} ${className || ""}`}
+        {...props}
+      ></input>
+      {error && <p className="font-bold col-danger mt-05 mb-0">{error}</p>}
+    </>
+  );
+};
+
+export default Input;
