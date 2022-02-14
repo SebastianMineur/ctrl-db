0fea3d0373e883235fcefb51e28c4e2365e1c7f2
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-02-14 11:40:22 +0100

    Enable use of 'ref' on Input

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import * as cls from "classnames";
 import css from "./css/Input.module.css";
 
-const Input = ({ className, error, ...props }) => {
+const Input = React.forwardRef(({ className, error, ...props }, ref) => {
   return (
     <>
       <input
         className={cls(css.Input, error && css.error, className)}
         {...props}
-      ></input>
+        ref={ref}
+      />
       {error && <p className="font-bold col-danger mt-05 mb-0">{error}</p>}
     </>
   );
-};
+});
 
 export default Input;
