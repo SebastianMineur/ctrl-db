f26a9af5e355bb3d526d496ba1e220864415b248
Sebastian Mineur <sebastian.mineur@gmail.com>
2022-02-14 11:39:22 +0100

    Move colors map to separate util file

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,20 +1,11 @@
 import * as cls from "classnames";
+import colors from "../util/colors";
 import css from "./css/Button.module.css";
 
 const variants = {
   none: "none",
   filled: "filled",
   outline: "outline",
-};
-
-const colors = {
-  primary: "var(--col-primary-hsl)",
-  light: "var(--col-light-hsl)",
-  dark: "var(--col-dark-hsl)",
-  danger: "var(--col-danger-hsl)",
-  warning: "var(--col-warning-hsl)",
-  success: "var(--col-success-hsl)",
-  info: "var(--col-info-hsl)",
 };
 
 const Button = ({ className, children, style, ...props }) => {
---
+++ frontend/src/util/colors.js
@@ -0,0 +1,9 @@
+export default {
+  primary: "var(--col-primary-hsl)",
+  light: "var(--col-light-hsl)",
+  dark: "var(--col-dark-hsl)",
+  danger: "var(--col-danger-hsl)",
+  warning: "var(--col-warning-hsl)",
+  success: "var(--col-success-hsl)",
+  info: "var(--col-info-hsl)",
+};
